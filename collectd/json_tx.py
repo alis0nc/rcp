7c399757aef70bbb3c6f65e3af1314cd430c4405
Alison Chan <alisonc@alisonc.net>
2014-10-28 17:04:42 -0400

    error handling and retry

--- collectd/json_tx.py
+++ collectd/json_tx.py
@@ -11,9 +11,7 @@ collectd python plugin to stream values over TCP in JSON format
 import collectd
 import socket
 import json
-
-host = '127.0.0.1'
-port = '36000'
+import threading
 
 """
 dictify: Translates object to dictionary
@@ -24,46 +22,82 @@ dictify: Translates object to dictionary
 def dictify(vl):
   return dict((n, getattr(vl, n)) for n in dir(vl) if not callable(getattr(vl, n)) and not n.startswith('__'))
 
-"""
-config: collectd config callback
-
-@param conf collectd.Config object
-"""
-def config(conf):
-  global host, port
-  for c in conf.children:
-    if c.key == 'Host':
-      host = c.values[0]
-    elif c.key == 'Port':
-      port = c.values[0]
-
-"""
-init: collectd init callback
-"""  
-def init():
-  global sock
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect((host, int(port)))
-
-"""
-write: collectd write callback
-
-@param vl collectd.Values to write
-"""
-def write(vl, data=None):
-  d = dictify(vl)
-  if sock:
-    sock.send(json.dumps(d))
+class JsonStreamer (object):
+  """
+  __init__
+  """
+  def __init__(self):
+    self.connected = False
+    self.sock = None
+    self.host = None
+    self.port = None
+    self.connect_timer = threading.Timer(30, self.try_connect)
     
-"""
-shutdown: collectd shutdown callback
-"""    
-def shutdown():
-  # gracefully clean up and close the socket
-  sock.shutdown(socket.SHUT_RDWR)
-  sock.close()
-    
-collectd.register_config(config)
-collectd.register_init(init)
-collectd.register_write(write)
-collectd.register_shutdown(shutdown)
+  """
+  try_connect: tries to connect to the server, sets up a retry timer if unsuccessful
+  """
+  def try_connect(self):
+    if not self.connected:
+      try:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, int(self.port)))
+        self.connected = True
+      except socket.error as ex:
+        # couldn't connect for some reason
+        self.connected = False
+        # log message
+        collectd.error("Couldn't connect to server; will retry in 30s (%s)" % str(ex))
+        if not self.connect_timer.is_alive():
+          self.connect_timer = threading.Timer(30, self.try_connect)
+          self.connect_timer.start()
+
+
+  """
+  config: collectd config callback
+
+  @param conf collectd.Config object
+  """
+  def config(self, conf):
+    for c in conf.children:
+      if c.key == 'Host':
+        self.host = c.values[0]
+      elif c.key == 'Port':
+        self.port = c.values[0]
+
+  """
+  init: collectd init callback
+  """  
+  def init(self):
+    self.try_connect()
+  """
+  write: collectd write callback
+
+  @param vl collectd.Values to write
+  """
+  def write(self, vl, data=None):
+    d = dictify(vl)
+    if self.sock and self.connected:
+      try:
+        self.sock.send(json.dumps(d))
+      except socket.error as ex:
+        # if we have an error, then we're not connected anymore
+        collectd.error("Failed to stream JSON; will try reconnecting (%s)" % str(ex))
+        self.connected = False
+        self.try_connect()
+      
+  """
+  shutdown: collectd shutdown callback
+  """    
+  def shutdown(self):
+    # gracefully clean up and close the socket
+    if self.sock and self.connected:
+      collectd.info("Caught shutdown callback; shutting down")
+      self.sock.shutdown(socket.SHUT_RDWR)
+      self.sock.close()
+
+
+streamer = JsonStreamer()      
+collectd.register_config(streamer.config)
+collectd.register_init(streamer.init)
+collectd.register_write(streamer.write)
+collectd.register_shutdown(streamer.shutdown)
